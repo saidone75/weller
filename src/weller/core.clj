9afbd476785b42905c23076106757b90b0cb49dd
saidone <saidone@saidone.org>
2024-05-02 08:14:24 +0200

    components

--- src/weller/core.clj
+++ src/weller/core.clj
@@ -1,9 +1,65 @@
 (ns weller.core
-  (:require [com.stuartsierra.component :as component])
+  (:require [clojure.data.json :as json]
+            [com.stuartsierra.component :as component]
+            [cral.utils.utils :as cu])
+  (:import (jakarta.jms Session)
+           (org.apache.activemq ActiveMQConnectionFactory))
   (:gen-class))
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+(defrecord ActiveMqListener
+  [config connection consumer]
+  component/Lifecycle
 
+  (start [this]
+    (println "starting ActiveMqListener")
+    (if connection
+      this
+      (let [connection-factory (new ActiveMQConnectionFactory)
+            _ (. connection-factory (setBrokerURL "tcp://localhost:61616"))
+            connection (.createConnection connection-factory)
+            session (.createSession connection false, Session/AUTO_ACKNOWLEDGE)
+            topic (.createTopic session "alfresco.repo.event2")
+            consumer (.createConsumer session topic)]
+        (.start connection)
+        (assoc this :connection connection :consumer consumer))))
+
+  (stop [this]
+    (println "stopping ActiveMqListener")
+    (.close connection)
+    (assoc this :connection nil :consumer nil)))
+
+(defn make-activemq-listener []
+  (component/using
+    (map->ActiveMqListener {})
+    []))
+
+(defn get-message [listener]
+  (let [message (.receive (:consumer listener))]
+    (println message)
+    (cu/kebab-keywordize-keys (json/read-str (.getText message)))))
+
+(defrecord Application [config activemq-listener]
+  component/Lifecycle
+
+  (start [this]
+    (println "Starting Application")
+    this)
+
+  (stop [this]
+    (println "Stopping Application")
+    this))
+
+(defn make-application [config]
+  (map->Application config))
+
+(defn application [config]
+  (component/system-map
+    :activemq-listener (make-activemq-listener)
+    :app (component/using
+           (make-application config)
+           [:activemq-listener])))
+
+(defn -main
+  [& args]
+  (let [component (component/start (application {}))]
+    (component/stop component)))
