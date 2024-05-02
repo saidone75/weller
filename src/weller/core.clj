ee3758ab1d5942d5451995a06d63130bbf395694
Marco Marini <mmarini@tai.it>
2024-05-02 10:51:05 +0200

    fix exit condition

--- src/weller/core.clj
+++ src/weller/core.clj
@@ -8,12 +8,15 @@
 
 (defn read-message [consumer status]
   (println "starting read-message")
-  (println consumer)
-  (println status)
-  (loop [message (.receive consumer)]
-    (println (cu/kebab-keywordize-keys (json/read-str (.getText message))))
-    (when (= status 'running)
-      (recur (.receive consumer))))
+  (loop [message nil]
+    (println @status)
+    (when-not (nil? message)
+      (println (cu/kebab-keywordize-keys (json/read-str (.getText message)))))
+    (when (:running @status)
+      (let [message (try (.receive consumer)
+                         (catch Exception e nil)
+                         (finally nil))]
+        (recur message))))
   (println "stopping read-message"))
 
 (defrecord ActiveMqListener
@@ -29,16 +32,18 @@
             connection (.createConnection connection-factory)
             session (.createSession connection false, Session/AUTO_ACKNOWLEDGE)
             topic (.createTopic session "alfresco.repo.event2")
-            consumer (.createConsumer session topic)]
+            consumer (.createConsumer session topic)
+            status (atom {})]
+        (swap! status assoc :running true)
         (.start connection)
-
         (.start (Thread. #(read-message consumer status)))
-        (assoc this :connection connection :consumer consumer))))
+        (assoc this :status status :connection connection :consumer consumer))))
 
   (stop [this]
     (println "stopping ActiveMqListener")
+    (swap! status assoc :running false)
     (.close connection)
-    (assoc this :status 'stopped :connection nil :consumer nil)))
+    (assoc this :connection nil :consumer nil)))
 
 (defn make-activemq-listener []
   (component/using
