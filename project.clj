3b45947bceec367d051cd390aabd74dd850b27d3
Marco Marini <mmarini@tai.it>
2024-05-08 10:04:42 +0200

    fix license url

--- project.clj
+++ project.clj
@@ -15,10 +15,10 @@
 ;  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 (defproject weller "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
-            :url  "https://www.eclipse.org/legal/epl-2.0/"}
+  :description "Alfresco out-of-process extensions in Clojure"
+  :url "https://saidone.org"
+  :license {:name "GNU General Public License v3.0"
+            :url  "https://www.gnu.org/licenses/gpl-3.0.txt"}
   :dependencies [[org.clojure/clojure "1.11.1"]
                  [com.stuartsierra/component "1.1.0"]
                  [org.apache.activemq/activemq-broker "6.1.1"]
